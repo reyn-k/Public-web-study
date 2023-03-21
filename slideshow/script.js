1fc07d4283a231b066c76ba6a731fe430689a70b
reyn-k <sv@rrrk.cc>
2023-03-21 19:19:52 +0900

    2023-03-21_js

--- slideshow/script.js
+++ slideshow/script.js
@@ -12,11 +12,26 @@
 // ];
 
 
+// addEventListenerの外でlet n
+// clickはアロー関数、引数渡さない
+// アロー関数の中でconsole.log(n) -> nの値を出力した。OK
+
+// addEventListenerの中でlet n
+// clickはアロー関数、引数渡さない
+// アロー関数の中でconsole.log(n) -> nの値を出力した。OK
+
+// addEventListenerの中でlet n
+// clickはアロー関数、引数はn
+// アロー関数の中でconsole.log(n) -> 出力した
+// [PointerEvent]というオブジェクト？が返された
+
+//続きは明日
 
 
     // プルリクする
-    // loadはwindow! documentではない
+    // loadはwinddocumentではない
     // document.addEventListener('load', () => { // 中は実行されない
+      console.log('a')
     window.addEventListener('DOMContentLoaded', () => {
       const list = [
       'images/01.jpg',
@@ -32,6 +47,12 @@
     ];
       const img = document.getElementById('main')
       const [arrowL, arrowR] = document.getElementsByTagName('span');
-      console.log(list)
-      // img.src = list[1];
+      // console.log(list)
+      img.src = list[imgIndex];
+      arrowL.addEventListener('click', function(img, imgIndex){
+        console.log(imgIndex)
+        img.src = list[imgIndex++];
+
+      })
+
     })
